5ccee1aca15b797bc943f500a74aa97f7419d885
MilesMachina <102100070@student.swin.edu.au>
2021-05-17 18:02:51 +1000

    Add Fading In Transition for Scenes

--- Doctor's Notes/Assets/Scripts/GameController.cs
+++ Doctor's Notes/Assets/Scripts/GameController.cs
@@ -27,6 +27,8 @@ public class GameController : MonoBehaviour
 
         blackCanvas = Instantiate((GameObject)Resources.Load("Prefabs/UI/BlackCanvas"), transform);
         blackCanvas.SetActive(false);
+
+        FadeInCoroutine(1f, 0.25f, true);
     }
 
     public void FadeOutCoroutine(float aDuration)
@@ -34,6 +36,13 @@ public class GameController : MonoBehaviour
         blackCanvas.SetActive(true);
         StartCoroutine(FadeOut(aDuration));
         StartCoroutine(sndManager.FadeOutMusic(aDuration));
+    }
+
+    public void FadeInCoroutine(float aDuration, float aVolume, bool aLoop = false)
+    {
+        blackCanvas.SetActive(true);
+        StartCoroutine(FadeIn(aDuration));
+        StartCoroutine(sndManager.FadeInMusic(aDuration, aVolume, aLoop));
     }
 
     IEnumerator FadeOut(float aDuration)
@@ -52,5 +61,24 @@ public class GameController : MonoBehaviour
             frame += Time.deltaTime;
             yield return null;
         }
+    }
+
+    IEnumerator FadeIn(float aDuration)
+    {
+        float frame = 0;
+        Image blackScreen = blackCanvas.transform.GetChild(0).gameObject.GetComponent<Image>();
+        Color c = blackScreen.color;
+        c.a = 1;
+        blackScreen.color = c;
+
+        while (frame < aDuration)
+        {
+            c.a -= (1 / aDuration) * Time.deltaTime;
+            blackScreen.color = c;
+
+            frame += Time.deltaTime;
+            yield return null;
+        }
+        blackCanvas.SetActive(false);
     }
 }
