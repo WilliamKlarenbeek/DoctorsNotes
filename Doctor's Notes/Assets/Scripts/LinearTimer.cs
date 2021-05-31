86146bb262abff83d617303e083215bb5e1cc0af
MilesMachina <102100070@student.swin.edu.au>
2021-06-01 00:08:42 +1000

    Relocate Databases + Add Blight Effect

--- Doctor's Notes/Assets/Scripts/LinearTimer.cs
+++ Doctor's Notes/Assets/Scripts/LinearTimer.cs
@@ -16,14 +16,32 @@ public class LinearTimer : MonoBehaviour
     //time spent by the player in the scene
     private TimeSpan timePlaying;
     //the time between each frame of the game
-    private float elapsedTime;
+    private float elapsedTime = 0f;
     //reset time is the number of seconds that we want to keep in a day
     //or however long we want the game's day to be.
-    public float resetTime; 
+    public float resetTime;
+
+    //Blight Effect
+    [SerializeField] private GameObject blightEffect;
+    private ParticleSystem blightParticleSystem;
+    private ParticleSystem.ShapeModule blightParticleSystemShape;
+    private ParticleSystem.EmissionModule blightParticleSystemEmission;
+
+    //The initial values of the blight effect (when the timer is zero)
+    private float blightOriginPointY = 4f;
+    private float blightOriginScaleY = 0f;
+    private float blightOriginEmission = 20f;
+    private float blightMaxYScale = 6f;
 
     private void Awake()
     {
         instance = this;
+        if (blightEffect != null)
+        {
+            blightParticleSystem = blightEffect.GetComponent<ParticleSystem>();
+            blightParticleSystemShape = blightParticleSystem.shape;
+            blightParticleSystemEmission = blightParticleSystem.emission;
+        }
     }
 
     // Start is called before the first frame update
@@ -36,7 +54,7 @@ public class LinearTimer : MonoBehaviour
     public void BeginTimer()
     {
         timerGoing = true;
-        elapsedTime = 0f;
+        //elapsedTime = 0f;
         timerBar.fillAmount = elapsedTime; 
 
         StartCoroutine(UpdateTimer()); 
@@ -59,6 +77,9 @@ public class LinearTimer : MonoBehaviour
             string timePlayingStr = timePlaying.ToString("mm':'ss'.'ff");
             timerBar.fillAmount = (float)(elapsedTime / resetTime);
             //timerBar.fillAmount = PlayerIcon.instance.distPercentage;
+            blightParticleSystemShape.position = new Vector2(0, Mathf.Lerp(blightOriginPointY, 0, (float)(elapsedTime / resetTime)));
+            blightParticleSystemShape.scale = new Vector3(10, Mathf.Lerp(blightOriginScaleY, blightMaxYScale, (float)(elapsedTime / resetTime)), 1);
+            blightParticleSystemEmission.rateOverTime = Mathf.Lerp(blightOriginEmission, 100f, (float)(elapsedTime / resetTime));
 
             if (elapsedTime == resetTime)
                 EndTimer();
