f8b6f737522f4d036643a14be713a2f2da6b7318
MilesMachina <102100070@student.swin.edu.au>
2021-11-06 23:41:56 +1100

    Fix Shader Graph

--- Doctor's Notes/Assets/Scripts/CinematicMap.cs
+++ Doctor's Notes/Assets/Scripts/CinematicMap.cs
@@ -7,12 +7,30 @@ public class CinematicMap : MonoBehaviour
     // Start is called before the first frame update
     void Start()
     {
-        
+
     }
 
     // Update is called once per frame
     void Update()
     {
         
+    }
+
+    public IEnumerator PlayEffect()
+    {
+        Debug.Log("Initializing Effect.");
+
+        float duration = 0f;
+        GetComponent<Renderer>().material.SetFloat("Phase", 0f);
+
+        while(duration < 60f)
+        {
+            duration += Time.deltaTime;
+            GetComponent<Renderer>().material.SetFloat("Phase", duration);
+
+            yield return null;
+        }
+        duration = 60f;
+        GetComponent<Renderer>().material.SetFloat("Phase", duration);
     }
 }
